63b3db4c321337320f53ab6793c7ff85b73c2357
Douglas Meehan <dmeehan@gmail.com>
2011-08-28 22:21:26 -0400

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+# blog/models.py  
+
+from django.conf import settings
+from django.db import models
+from django.db.models import permalink
+
+from taggit.managers import TaggableManager
+import categories
+
+from blocks.models import LinkBase, ArticleBase
+from images.models import RelatedImageAutoBase
+
+class Link(LinkBase):
+    """
+        A link entry for the blog
+    """
+    # taxonomy
+    tags = TaggableManager(blank=True)
+    categories = models.ManyToManyField('categories.Category')
+
+
+class Entry(ArticleBase):
+    """
+        An article entry for the blog
+    """
+    enable_comments = models.BooleanField(default=True)
+    links = models.ManyToManyField(Link)
+
+    # taxonomy
+    tags = TaggableManager(blank=True)
+    categories = models.ManyToManyField('categories.Category')
+
+
+class EntryImage(RelatedImageAutoBase):
+    """
+        Images for a blog entry.
+    """
+    entry = models.ForeignKey(Entry)
+
+
+
+class LinkImage(RelatedImageAutoBase):
+    """
+        Images for a link.
+    """
+    link = models.ForeignKey(Link)
