8c1c2c5ec30efb15eeeded1b0b5dc6b5e6636512
Douglas Meehan <dmeehan@gmail.com>
2011-11-26 17:23:47 -0500

    added prepopulated fields to admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
